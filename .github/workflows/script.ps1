f1c478f50edef12181490a0085d36eb1902e3ab4
Fralacticus <50240332+Fralacticus@users.noreply.github.com>
2022-08-14 19:10:15 +0200

    Update script.ps1

--- .github/workflows/script.ps1
+++ .github/workflows/script.ps1
@@ -1,3 +1,16 @@
+<#
+    .SYNOPSIS
+    This script generates a password
+    .DESCRIPTION
+    A password is generated that contains at least one  non-capital, one capital, one number and one special character.
+    It will than removes the similar characters (like I & l)
+    .PARAMETER liste
+    The length of the password
+    .NOTES
+    Written by Barbara Forbes
+    @Ba4bes
+    https://4bes.nl
+    #>
 param(
     [parameter(Mandatory = $true)]
     $listeFichiers
