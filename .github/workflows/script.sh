650a6646af800a5e0930f4517200f1684ccb5cba
Fralacticus <50240332+Fralacticus@users.noreply.github.com>
2022-08-15 08:24:43 +0200

    Update and rename script.ps1 to script.sh

---
+++ .github/workflows/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#########
+# some debug output to check in case of failures
+# also exit on any error
+set -xe
+
+cd $1
+(
+cat $1 |  echo
+
+) > Summary.md
+
+git config --global user.email "fralacticus@gmail.com"
+git config --global user.name "fralacticus"
+git add Summary.md
+git commit -m "Update Summary.md at $(date)"
+git push origin master
