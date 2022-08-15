8f4074bdab665e000261b3d993dfc68e5a07b2a6
Fralacticus <50240332+Fralacticus@users.noreply.github.com>
2022-08-15 09:14:30 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -10,10 +10,11 @@ set -xe
   #echo "dossier : $(dirname $i)"
 #done
 
-
+dirname= dirname "$i"
+foldername= basename "$dirname"
 echo "partie principale: "
 #cd $1
-cat $1 > "$(dirname $i)/Summary.md"
+cat $1 > "foldername/Summary.md"
 
 git config --global user.email "fralacticus@gmail.com"
 git config --global user.name "fralacticus"
