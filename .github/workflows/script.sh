b24fbfaf6bc2bd9e2b0c3549de06a0559d16c85e
Fralacticus <50240332+Fralacticus@users.noreply.github.com>
2022-08-15 11:28:50 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
 #########
-# some debug output to check in case of failures
-# also exit on any error
+# Affiche chaque commande exécutée avec ses arguments
+# Sort du script dès qu’une erreur est rencontrée (retour différent de 0)
 set -xe
 
-#for i in $(find . -name '*_brut.md') ; do
-  #echo "$i"
-  #echo "dossier : $(dirname $i)"
-#done
+echo "------- Début du script --------"
 
-# Définition du nom du nouveau fichier
+echo "-> On définie le du nom du nouveau fichier à partir de l'original en retirant le suffixe _brut"
 destFic="$(echo $1 | sed "s/_brut//g")"
 
+echo "-> On définie les motifs pour convertir les liens d'images relatifs au dossier assets en absolus"
 ancien='(assets/'
 nouveau="(https://raw.githubusercontent.com/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY##*/}/${GITHUB_REF_NAME}/$(dirname $1)/assets/"
 
 ancien2='src='\"'assets/'
 nouveau2='src='\""https://raw.githubusercontent.com/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY##*/}/${GITHUB_REF_NAME}/$(dirname $1)/assets/"
 
-# Notice sed -> s/motif/substitut/g
-
-echo "partie principale: "
-#cd $1
+echo "-> On remplace les motifs et on écrie le nouveau fichier"
 cat $1 | sed "s|${ancien}|${nouveau}|g" | sed "s|${ancien2}|${nouveau2}|g" > $destFic
 
-
-
+echo "-> On met à jour le dépôt git avec ajout ou écrasement du nouveau fichier"
 git config --global user.email "fralacticus@gmail.com"
 git config --global user.name "fralacticus"
 git add $destFic
 git commit -m "Mise à jour $($1) le $(date)"
 git push origin main
+
+echo "------- Fin du script --------"
