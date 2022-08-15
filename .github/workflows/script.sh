2baaffee8596538de0cc91008bdb48f83587b85e
Fralacticus <50240332+Fralacticus@users.noreply.github.com>
2022-08-15 09:38:56 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -13,7 +13,9 @@ set -xe
 
 echo "partie principale: "
 #cd $1
-cat $1 > $2
+cat $1 | \
+  sed 's/\(assets\//https:\/\/raw.githubusercontent.com\/Fralacticus\/articles_md\/main/$(dirname $1)/assets\/)/g' | \
+  > $2
 
 git config --global user.email "fralacticus@gmail.com"
 git config --global user.name "fralacticus"
