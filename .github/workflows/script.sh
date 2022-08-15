e6c34a44f459e60b7948a72b98aed9e373817854
Fralacticus <50240332+Fralacticus@users.noreply.github.com>
2022-08-15 11:20:08 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -10,6 +10,9 @@ set -xe
   #echo "dossier : $(dirname $i)"
 #done
 
+# Définition du nom du nouveau fichier
+destFic="$(echo $1 | sed "s/_brut//g")"
+
 ancien='(assets/'
 nouveau="(https://raw.githubusercontent.com/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY##*/}/${GITHUB_REF_NAME}/$(dirname $1)/assets/"
 
@@ -20,10 +23,12 @@ nouveau2='src='\""https://raw.githubusercontent.com/${GITHUB_REPOSITORY_OWNER}/$
 
 echo "partie principale: "
 #cd $1
-cat $1 | sed "s|${ancien}|${nouveau}|g" | sed "s|${ancien2}|${nouveau2}|g" > $2
+cat $1 | sed "s|${ancien}|${nouveau}|g" | sed "s|${ancien2}|${nouveau2}|g" > $destFic
+
+
 
 git config --global user.email "fralacticus@gmail.com"
 git config --global user.name "fralacticus"
-git add $2
+git add $destFic
 git commit -m "Mise à jour $($1) le $(date)"
 git push origin main
