3b345731d15e0144eea096c37a206ad22bd586dc
Fralacticus <50240332+Fralacticus@users.noreply.github.com>
2022-08-15 11:12:07 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -12,16 +12,18 @@ set -xe
 
 ancien='(assets/'
 nouveau="(https://raw.githubusercontent.com/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY##*/}/${GITHUB_REF_NAME}/$(dirname $1)/assets/"
-#nouveau='https://raw.githubusercontent.com/Fralacticus/articles_md/main/$(dirname $1)/assets/'
+
+ancien2='src='\"'assets/'
+nouveau2='src='\""https://raw.githubusercontent.com/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY##*/}/${GITHUB_REF_NAME}/$(dirname $1)/assets/"
 
 # Notice sed -> s/motif/substitut/g
 
 echo "partie principale: "
 #cd $1
-cat $1 | sed "s|${ancien}|${nouveau}|g" > $2
+cat $1 | sed "s|${ancien}|${nouveau}|g" | sed "s|${ancien2}|${nouveau2}|g" > $2
 
 git config --global user.email "fralacticus@gmail.com"
 git config --global user.name "fralacticus"
 git add $2
-git commit -m "Update Summary.md at $(date)"
+git commit -m "Mise à jour $($1) le $(date)"
 git push origin main
