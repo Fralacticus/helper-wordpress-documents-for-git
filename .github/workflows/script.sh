40f0e96759168d225a25302bc1ef64ebb3dc0552
Fralacticus <50240332+Fralacticus@users.noreply.github.com>
2022-08-15 10:48:30 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -11,7 +11,7 @@ set -xe
 #done
 
 ancien='(assets/'
-nouveau="(https://raw.githubusercontent.com/Fralacticus/${GITHUB_WORKSPACE}/main/$(dirname $1)/assets/"
+nouveau="(https://raw.githubusercontent.com/${GITHUB_REPOSITORY_OWNER}/articles_md/main/$(dirname $1)/assets/"
 #nouveau='https://raw.githubusercontent.com/Fralacticus/articles_md/main/$(dirname $1)/assets/'
 
 # Notice sed -> s/motif/substitut/g
